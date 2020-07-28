f250308e50898f83db577c64466290eda419adf3
aljanabim <ma970101@yahoo.se>
2020-07-29 00:03:30 +0200

    Fixed original plot colouring

--- detector.py
+++ detector.py
@@ -3,9 +3,9 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('./static/signs/VM-E20-1_960x960.png')
+img = cv.imread('./signs/VM-E20-1_960x960.png')
 edges = cv.Canny(img, 100, 200)
-plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.subplot(121), plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122), plt.imshow(edges, cmap='gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
