8b78469dd15652149908a0a2e01e93dee7c80f76
aljanabim <ma970101@yahoo.se>
2020-07-29 00:28:12 +0200

    Refactored code in functions + Added corener detection

--- detector.py
+++ detector.py
@@ -1,13 +1,37 @@
 
 import numpy as np
-import cv2 as cv
+import cv2
 from matplotlib import pyplot as plt
 
-img = cv.imread('./signs/VM-E20-1_960x960.png')
-edges = cv.Canny(img, 100, 200)
-plt.subplot(121), plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(edges, cmap='gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
-print("hello world")
+
+def edge_detection(filename):
+    img = cv2.imread()
+    edges = cv2.Canny(img, 100, 200)
+    plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(edges, cmap='gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+def corner_detection(filename):
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+
+    # result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst, None)
+
+    # Threshold for an optimal value, it may vary depending on the image.
+    img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
+    cv2.imshow('dst', img)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    img_file = './signs/VM-E20-1_960x960.png'
+    corner_detection(img_file)
